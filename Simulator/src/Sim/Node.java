8c823dd3126f2966d799dd8dcf1133194073aa29
Unknown <anduun@live.se>
2017-03-08 14:53:31 +0100

    Fixed the failfix xD

--- Simulator/src/Sim/Node.java
+++ Simulator/src/Sim/Node.java
@@ -66,12 +66,6 @@ public class Node extends SimEnt {
 		send(this, new TimerEvent(),0);	
 	}
 	
-	// timeToMove is the sim time when the Node should move back to its home network
-	public void moveBackHome(int timeToMove)
-	{
-		// Send Deregistration message to HA and update links etc
-		send(_homeAgent, new Deregistration(this), timeToMove);
-	}
 	
 	public void startSendingTCP(int network, int node, int number, int timeInterval)
 	{
@@ -81,7 +75,6 @@ public class Node extends SimEnt {
 		_toHost = node;
 		int seqNr = rand.nextInt(101);
 		int ackNr = rand.nextInt(11);
-		_trafficDistributionType = 0;
 		send(this, new MessageTCP(_id, new NetworkAddr(_toNetwork, _toHost), seqNr, ackNr, 0 , 1 ), 0);
 	}
 	
@@ -105,50 +98,6 @@ public class Node extends SimEnt {
 		if (ev instanceof Message)
 		{
 			System.out.println("Node "+_id.networkId()+ "." + _id.nodeId() +" receives message with seq: "+((Message) ev).seq() + " at time "+SimEngine.getTime());
-			
-		}
-		else if (ev instanceof AgentAdvertisement)
-		{
-			
-			System.out.println();
-			System.out.println("Node "+_id.networkId()+ "." + _id.nodeId() +" receives Agent Advertisement at time "+SimEngine.getTime());
-			System.out.println("Change Network ID from "+_id.networkId()+" to "+((AgentAdvertisement) ev).getNewNetworkID());
-			System.out.println("Send a Registration Request with old Network ID "+_id.networkId()+" to Home Agent");
-			System.out.println();
-			
-			// oldNetworkID is used so the Node can identify itself at its Home Agent
-			int oldNetworkID = _id.networkId();
-			
-			// Handle the AgentAdvertisement things (Care-of Address)
-			_id = new NetworkAddr(((AgentAdvertisement) ev).getNewNetworkID(), _id.nodeId());
-			
-			// Send a Registration Request to Home Agent
-			// Registration Request can include a Lifetime parameter (not included in our solution)
-			send(_homeAgent, new RegistrationRequest(this, oldNetworkID), 0);
-		}
-		else if (ev instanceof RegistrationReply)
-		{
-			// Registration to Home Agent is ok
-			onForeignNetwork = true;
-			
-			System.out.println();
-			System.out.println("Node "+_id.networkId()+ "." + _id.nodeId() +" received Registration Reply at time "+SimEngine.getTime());
-			System.out.println();
-			
-			// Set Lifetime value (not included in our solution)
-		}
-		else if (ev instanceof DeregistrationReply)
-		{
-			System.out.println();
-			System.out.println("Node "+_id.networkId()+"."+_id.nodeId()+" received Deregistration Reply at time "+SimEngine.getTime());
-			System.out.println("Change Network ID from "+_id.networkId()+" to "+((DeregistrationReply) ev).getOldNetworkID());
-			System.out.println();
-			_id = new NetworkAddr(((DeregistrationReply) ev).getOldNetworkID(), _id.nodeId());
-			onForeignNetwork = false;
-		}
-		else if	(ev instanceof MessageTCP)
-		{
-			System.out.println("TCP HYPE");
 			
 		}
 	}
